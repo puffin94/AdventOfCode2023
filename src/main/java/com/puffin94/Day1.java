827f3bd0175e0108ec50729fbf2129dbe41a8a76
puffin94 <65535216+puffin94@users.noreply.github.com>
2023-12-01 09:51:03 +0000

    Day1 1 star

--- src/main/java/com/puffin94/Day1.java
+++ src/main/java/com/puffin94/Day1.java
@@ -1,7 +1,98 @@
 package com.puffin94;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Day1 {
     public static void main(String[] args) {
+        ArrayList<CalibrationValue> calibrationValues = new ArrayList<>();
+        try {
+            FileReader fr = new FileReader("src/main/resources/challengeInputs/day1.txt");
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                calibrationValues.add(new CalibrationValue(line));
 
+            }
+        } catch (Exception e) {
+            System.err.println("Issue Reading Challenge file");
+            System.err.println(e);
+        }
+
+        System.out.println("Summing all calibration Values...");
+        int sumOfValues = 0;
+        int updatedSum = 0;
+        for (CalibrationValue cv : calibrationValues) {
+            sumOfValues += cv.getCalibrationValue();
+            updatedSum += cv.getUpdatedCalibrationValue();
+        }
+        System.out.println("Value of all calibration values is :" + sumOfValues);
+        System.out.println("Updated value of all calibration values is :" + updatedSum);
+    }
+
+    static class CalibrationValue {
+        private String[] valuesArray;
+        private String valueString;
+        private String calibrationValue;
+        private String updatedCV;
+
+        private String[] numbersAsWords = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        public CalibrationValue(String values) {
+            valueString = values;
+            this.valuesArray = values.split("");
+            calibrationValue = assignCalibrationValues(valuesArray);
+            updatedCV = assignCalibrationValues(replaceWordsAsNumbers().split(""));
+        }
+
+        private String assignCalibrationValues(String[] arrayOfValues) {
+            String value1 = null, value2 = null;
+            for (String character : arrayOfValues) {
+                try {
+                    if (character == null) {
+                        continue;
+                    }
+                    Integer.parseInt(character);
+                } catch (NumberFormatException e) {
+                    continue;
+                }
+                value1 = character;
+                break;
+            }
+            for (int i = arrayOfValues.length - 1; i >= 0; i--) {
+                try {
+                    if (arrayOfValues[i] == null) {
+                        continue;
+                    }
+                    Integer.parseInt(arrayOfValues[i]);
+                } catch (NumberFormatException e) {
+                    continue;
+                }
+                value2 = arrayOfValues[i];
+                break;
+            }
+
+            return value1 + value2;
+        }
+
+        public Integer getCalibrationValue() {
+            return Integer.parseInt(calibrationValue);
+        }
+
+        public Integer getUpdatedCalibrationValue() {
+            return Integer.parseInt(updatedCV);
+        }
+
+        // needs modded to pick up the last and first most number when they conflict i.e. twone should be tw1 or v/v
+        private String replaceWordsAsNumbers() {
+            String replacementValue = valueString;
+            for (int i = numbersAsWords.length-1;i>=0 ;i--) {
+                while(replacementValue.contains(numbersAsWords[i])){
+                    replacementValue = replacementValue.replace(numbersAsWords[i],String.valueOf(i+1));
+                }
+            }
+            return replacementValue;
+        }
     }
 }
