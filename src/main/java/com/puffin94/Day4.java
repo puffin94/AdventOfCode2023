ae4e6094419df20ea043c4fb6f842b410e1c2001
puffin94 <65535216+puffin94@users.noreply.github.com>
2023-12-19 14:36:28 +0000

    Day 4 part 2

--- src/main/java/com/puffin94/Day4.java
+++ src/main/java/com/puffin94/Day4.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Day4 {
@@ -14,20 +13,23 @@ public class Day4 {
             BufferedReader br = new BufferedReader(fr);
             String line;
             int totalPoints = 0;
+            List<Card> cardList = new ArrayList<>();
             while ((line = br.readLine()) != null) {
                 String[] split = line.split(":");
-                String cardName = split[0];
+                String cardNumber = split[0];
                 String wals = split[1];
                 totalPoints += countPoints(wals);
+                cardList.add(new Card(cardNumber, wals));
             }
             System.out.println("Total Points: " + totalPoints);
+            System.out.println("Total Cards: "+ countCards(cardList));
         } catch (Exception e) {
             System.err.println("Issue Reading Challenge file");
             System.err.println(e);
         }
     }
 
-    public static int countPoints(String wals) {
+    public static int countNumberOfMatches(String wals) {
         int numberOfMatches = 0;
         String[] split = wals.split("\\|");
         List<String> winners = new ArrayList<>(Arrays.asList(split[0].split(" ")));
@@ -40,11 +42,69 @@ public class Day4 {
                 numberOfMatches++;
             }
         }
+        return numberOfMatches;
+    }
 
+    public static int countPoints(String wals) {
+        int numberOfMatches = countNumberOfMatches(wals);
         if (numberOfMatches > 0) {
             return (int) Math.pow(2, (numberOfMatches - 1));
         }
         return numberOfMatches;
+    }
 
+    public static int countCards(List<Card> cardList) {
+        int copyCount = 0;
+        for (int i = 0; i < cardList.size(); i++) {
+            Card card = cardList.get(i);
+            if (card.getNumberOfWinners() > 0) {
+                for (int j = 0; j < card.getCopyCount(); j++) {
+                    for (int l = 0; l < card.getNumberOfWinners(); l++) {
+                        cardList.get(i + l + 1).increaseCopyCount();
+                    }
+                }
+            }
+        }
+
+        for (Card card : cardList) {
+            copyCount += card.getCopyCount();
+        }
+        return copyCount;
+    }
+
+    public static class Card {
+        private int cardNumber;
+        private int numberOfWinners;
+        private int copyCount = 1;
+
+        public Card(String cardNumber, String wals) {
+            setCardNumber(cardNumber);
+            setNumberOfMatches(wals);
+        }
+
+        private void setNumberOfMatches(String wals) {
+            this.numberOfWinners = countNumberOfMatches(wals);
+        }
+
+        private void setCardNumber(String cardNumber) {
+            String[] split = cardNumber.split(" ");
+            this.cardNumber = Integer.parseInt(split[split.length - 1]);
+        }
+
+        public void increaseCopyCount() {
+            this.copyCount++;
+        }
+
+        public int getCardNumber() {
+            return this.cardNumber;
+        }
+
+        public int getNumberOfWinners() {
+            return this.numberOfWinners;
+        }
+
+        public int getCopyCount() {
+            return this.copyCount;
+        }
     }
 }
